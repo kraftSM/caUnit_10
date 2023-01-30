9159f704dc5bd35a763a6706e303e6f5416fff63
kraftSM <kraftsm@yandex.ru>
2023-01-30 12:51:28 +0300

    Ini Version

---
+++ caUnit10Hw1-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caUnit10Hw1_2
+{
+    internal class Program
+    {
+        static string UnitName = "Un:10";
+        static string UnDescr = "Interface.";
+        static string ExName = "HW-1/2 Calculator";
+        static string ExDescr = "Calculator";
+        static protected string UnTitle
+        {
+            get
+            { return UnitName + "[" + UnDescr + "]"; }
+        }
+        static protected string ExTitle
+        {
+            get
+            { return UnitName + "." + ExName + "[" + ExDescr + "]"; }
+        }
+        static protected string Promt
+        {
+            get
+            { return UnitName + "." + ExName + ":"; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}|{1}:Startig ", UnTitle, ExTitle);
+            Console.WriteLine("{0} ", Promt);
+            //
+
+            Console.WriteLine("\n{0}: Finishing.", ExTitle);
+            Console.WriteLine("{0}: Finished.\nPress any key.", UnTitle);
+            Console.ReadKey();
+        }
+    }
+}
